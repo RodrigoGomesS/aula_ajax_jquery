bf680d91efba4acf5c0e597cafa6199a5ce625ed
Rodrigo dos Santos Gomes <cklrod@hotmail.com>
2021-03-30 19:40:37 -0300

    exercicio de como usar instalar jquery e usar ajax

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function consultaCep() {
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep + "/json/";
+
+    console.log(url);
+
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function (response) {
+            console.log(response);
+        }
+    })
+}
