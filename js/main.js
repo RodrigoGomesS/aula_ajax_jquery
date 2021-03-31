f87980fdfe4369ba5bd78d8fdfd0d11016e79458
Rodrigo Gomes <cklrod@hotmail.com>
2021-03-31 15:48:43 -0300

    trabalhando com css, ajax e jquery, melhorando arquivo,

--- js/main.js
+++ js/main.js
@@ -9,6 +9,14 @@ function consultaCep() {
         type: "GET",
         success: function (response) {
             console.log(response);
+            $("#logradouro").html(response.logradouro);
+            //document.getElementById("logradouro").innerHTML = response.logradouro;
+            $("#bairro").html(response.bairro);
+            //document.getElementById("bairro").innerHTML = response.bairro;
+            $("#localidade").html(response.localidade);
+            //document.getElementById("localidade").innerHTML = response.localidade;
+            $("#uf").html(response.uf);
+            //document.getElementById("uf").innerHTML = response.uf;
         }
     })
 }
